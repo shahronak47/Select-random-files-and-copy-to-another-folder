418748dd05b6ccc1bfbc24aec8d300f77ebaddaf
Ronak Shah <shahronak47@yahoo.in>
2017-09-06 23:35:36 +0800

    optimized the code with full.name

--- random_files.R
+++ random_files.R
@@ -1,8 +1,8 @@
 get_random_files <- function(n) {
   #list.files lists all the files in the directory and sample selects n random enteries in it
-  files = sample(list.files("outermost_directory_path", recursive = TRUE), n)
+  files = sample(list.files("outermost_directory_path", recursive = TRUE, full.names = TRUE), n)
   #filter only those files which are required, here ending with mkv
-  filepath = paste0("complete_directory_path_name", files[grepl("mkv$", files)])
+  filepath = files[grepl("mkv$", files)]
   #copy the files to your desired directory
   file.copy(filepath, to = "your_directory_path")
 }
